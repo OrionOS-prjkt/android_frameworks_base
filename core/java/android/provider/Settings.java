d94620fa0deb7b225074866af5fe2f13920d48b1
TikkiTikki <radstylemusic@gmail.com>
2021-04-18 19:37:06 -0300

    Pulse: Vertical mirror [1/2]
    Change-Id: I4251534e88a8a88c0af418116c30e86bcda405ab
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13418,6 +13418,12 @@ public final class Settings {
          * @hide
          */
         public static final String VISUALIZER_CENTER_MIRRORED = "visualizer_center_mirrored";
+        
+        /**
+         * Pulse vertical mirror
+         * @hide
+         */
+        public static final String PULSE_VERTICAL_MIRROR = "pulse_vertical_mirror";    
 
         /**
          * Volume styles
