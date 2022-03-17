209f14430fb839f2c1f5dc29e4983f7918c9d32c
LibXZR <i@xzr.moe>
2022-03-17 22:58:19 +0800

    base: Add support for application downgrade [1/2]
    Can be useful in some cases.
    
    Change-Id: I999337cc55e9675cedde35191a6438d90744e5aa
    Signed-off-by: LibXZR <i@xzr.moe>
    
    Adapt to Android 13
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -20227,6 +20227,12 @@ public final class Settings {
          * @hide
          */
         public static final String RESTRICTED_NETWORKING_MODE = "restricted_networking_mode";
+        
+        /**
+         * Control whether application downgrade is allowed.
+         * @hide
+         */
+        public static final String PM_DOWNGRADE_ALLOWED = "pm_downgrade_allowed";
 
         /**
          * Control whether to spoof current status of storage encryption.
