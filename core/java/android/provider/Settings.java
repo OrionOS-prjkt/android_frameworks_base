4e620341e06052435bd6fa92473143af0f97fa47
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-12 08:18:44 +0800

    base: Make Gesture navbar IME space settings key readable
    * to prevent third-party keyboards from crashing
    
    cherrypicked from thirteen
    
    Original commit:
    "Make Hide Ime Space settings key readable"
    https://github.com/minaripenguin/android_frameworks_base/commit/be7427538cb4b2b3a3de4210ea07378d8a47f747
    
    Change-Id: Ie48081c8a365abd18e4b6f978c08654cbe32d132
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7067,6 +7067,7 @@ public final class Settings {
         /**
          * @hide
          */
+        @Readable
         public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
 
         /**
