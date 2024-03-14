f0e2ea6b22ed63cbeb6dd4511f2940785b4ebc50
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-14 18:48:07 +0800

    SystemUI: Implement intensity-based brightness slider haptics
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -63,6 +63,7 @@ import dagger.assisted.AssistedFactory;
 import dagger.assisted.AssistedInject;
 
 import java.util.concurrent.Executor;
+import com.android.internal.util.android.VibrationUtils;
 
 public class BrightnessController implements ToggleSlider.Listener, MirroredBrightnessController,
         TunerService.Tunable {
@@ -117,7 +118,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private Vibrator mVibrator;
     private static final VibrationEffect BRIGHTNESS_SLIDER_HAPTIC =
             VibrationEffect.get(VibrationEffect.EFFECT_TICK);
-    private boolean mBrightnessSliderHaptic;
+    private int mBrightnessSliderHaptic;
 
     @Override
     public void setMirror(BrightnessMirrorController controller) {
@@ -135,7 +136,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
                 break;
             case QS_BRIGHTNESS_SLIDER_HAPTIC:
                 mBrightnessSliderHaptic =
-                        TunerService.parseIntegerSwitch(newValue, false);
+                        TunerService.parseInteger(newValue, 0);
                 break;
             default:
                 break;
@@ -369,8 +370,8 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         setBrightness(valFloat);
 
         // Give haptic feedback only if brightness is changed manually
-        if (mBrightnessSliderHaptic && mVibrator != null && tracking)
-            mVibrator.vibrate(BRIGHTNESS_SLIDER_HAPTIC);
+        if (mBrightnessSliderHaptic > 0 && mVibrator != null && tracking)
+            VibrationUtils.triggerVibration(mContext, mBrightnessSliderHaptic);
 
         if (!tracking) {
             AsyncTask.execute(new Runnable() {
