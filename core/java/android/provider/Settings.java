ebeab3bf2b9909b9406890624cecc58a0449015a
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-14 18:30:27 +0800

    SystemUI: Implement intensity-based qs tiles haptic feedback
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7035,6 +7035,11 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_FONT_STYLE = "status_bar_clock_font_style";
 
         /**
+         * @hide
+         */
+        public static final String QS_PANEL_TILE_HAPTIC = "qs_panel_tile_haptic";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
