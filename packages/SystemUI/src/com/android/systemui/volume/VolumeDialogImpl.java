29f68f425bdbee9b7aeeb463fe68c83c2fc9f5fc
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-11 12:47:14 +0800

    VolumeDialogImpl: Separate volume sound and vibration haptics
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -154,6 +154,8 @@ import dagger.Lazy;
 
 import lineageos.providers.LineageSettings;
 
+import com.android.internal.util.android.VibrationUtils;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -3059,7 +3061,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
-            mVolumeUtils.playSoundForStreamType(mRow.stream);
+            int vibrateIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.VOLUME_SLIDER_HAPTICS_INTENSITY, 1);
+            VibrationUtils.triggerVibration(mContext, vibrateIntensity);
         }
 
         @Override
@@ -3080,6 +3084,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                 mHandler.sendMessageDelayed(mHandler.obtainMessage(H.RECHECK, mRow),
                         USER_ATTEMPT_GRACE_PERIOD);
             }
+            mVolumeUtils.playSoundForStreamType(mRow.stream);
         }
     }
 
