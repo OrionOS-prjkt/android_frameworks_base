2fb39ecd2d371b17aa83e194178b34984cb664be
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-16 07:30:41 +0800

    Revert Dialog dismiss changes [SQUASH]
    reason for revert: doesn't fix the volume panel positioning/gravity bug
    
    Revert "VolumeDialogImpl: Dismiss dialog on config change"
    
    This reverts commit 6c65fffd144f985634f09a5f295ab8885004ce18.
    
    Revert "VolumeDialog: Ensure proper resource release"
    
    This reverts commit b365d7f35cc221f56eec581cefefa1e5011d34a4.
    
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -507,11 +507,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     @Override
     public void onUiModeChanged() {
-        if (mDialog != null) {
-            mDialog.dismiss();
-        }
         mContext.getTheme().applyStyle(mContext.getThemeResId(), true);
-        mConfigChanged = true;
     }
 
     public void init(int windowType, Callback callback) {
@@ -645,13 +641,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private void initDialog(int lockTaskModeState) {
         Log.d(TAG, "initDialog: called!");
-        if (mDialog != null) {
-            mDialog.dismiss();
-            mDialog = null;
-        }
-        if (mConfigurableTexts != null) {
-            mConfigurableTexts = null;
-        }
         mDialog = new CustomDialog(mContext);
         initDimens();
         adjustPositionOnScreen();
@@ -2960,11 +2949,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     @Override
     public void onConfigChanged(Configuration config) {
-        if (mDialog != null) {
-            mDialog.dismiss();
-        }
         mOrientation = config.orientation;
-        mConfigChanged = true;
     }
 
     private final VolumeDialogController.Callbacks mControllerCallbackH
@@ -2995,9 +2980,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         @Override
         public void onConfigurationChanged() {
-            if (mDialog != null) {
-                mDialog.dismiss();
-            }
+            mDialog.dismiss();
             mConfigChanged = true;
         }
 
