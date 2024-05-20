93aed6e62b958d91a506968f8c8c0b41fb914218
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-20 21:10:16 +0800

    SystemUI: Optimize notification expansion animation
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2781,7 +2781,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (mIsOcclusionTransitionRunning) {
             return;
         }
-
+        if (mQsController.isExpandImmediate() && !mQsController.getFullyExpanded()) {
+            mNotificationStackScrollLayoutController.setMaxAlphaForExpansion(0f);
+            return;
+        }
         if (!KeyguardShadeMigrationNssl.isEnabled()) {
             float alpha = 1f;
             if (mClosingWithAlphaFadeOut && !mExpandingFromHeadsUp
@@ -2793,9 +2796,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 && !mQsController.getFullyExpanded()) {
                 alpha *= mClockPositionResult.clockAlpha;
             }
-            if (mQsController.isExpandImmediate() && !mQsController.getFullyExpanded()) {
-                alpha = 0f;
-            }
+            alpha = (alpha < 0.88f) ? 0f : 1f;
             mNotificationStackScrollLayoutController.setMaxAlphaForExpansion(alpha);
         }
     }
