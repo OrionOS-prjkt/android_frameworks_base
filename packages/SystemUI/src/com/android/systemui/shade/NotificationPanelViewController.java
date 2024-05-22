1513e64174770549650820940ba02e2e6b5f2583
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-22 18:07:21 +0800

    SystemUI: Use VibrationUtils for QS/Status bar haptics
    * VibrationsUtils uses single thread executor compared to
      vibratorhelper's performHapticFeedback,single thread seems to play nicer
      with animations compared to async/normal vibration callbacks based from our tests.
      We initially observed this when upstreaming VibrationUtils coming from async tasks
      where we observed hiccups/lags when triggering vibrations
      for edge back gesture animation.
    
    test: swipe QS panel, no hiccups observed when expanding QS with media player
    which is better compared to performHapticFeedback where we observed a bit
    of hiccups especially when qs media player is visible
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -241,6 +241,7 @@ import com.android.wm.shell.animation.FlingAnimationUtils;
 import lineageos.providers.LineageSettings;
 
 import android.util.RisingBoostFramework;
+import com.android.internal.util.android.VibrationUtils;
 
 import dalvik.annotation.optimization.NeverCompile;
 
@@ -3916,10 +3917,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private void maybeVibrateOnOpening(boolean openingWithTouch) {
         if (mVibrateOnOpening && mBarState != KEYGUARD && mBarState != SHADE_LOCKED) {
             if (!openingWithTouch || !mHasVibratedOnOpen) {
-                mVibratorHelper.performHapticFeedback(
-                        mView,
-                        HapticFeedbackConstants.GESTURE_START
-                );
+                VibrationUtils.triggerVibration(mView.getContext(), 2);
                 mHasVibratedOnOpen = true;
                 mShadeLog.v("Vibrating on opening, mHasVibratedOnOpen=true");
             }
