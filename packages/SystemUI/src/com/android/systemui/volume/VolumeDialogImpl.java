c8702c4320e475871dbb111b7f40c0bb5ab43386
minaripenguin <minaripenguin@users.noreply.github.com>
2024-06-03 08:10:33 +0800

    VolumeDialogImpl: Fix volume panel horizontal translation
    * there were some scenarios where volume panel skips the animation, this results to translationX being set to 0, which results to the center panel translation bug
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -706,8 +706,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mDialog.setOnShowListener(dialog -> {
             mDialogView.getViewTreeObserver().addOnComputeInternalInsetsListener(this);
             if (!shouldSlideInVolumeTray()) {
-                mDialogView.setTranslationX(
-                        (isWindowGravityLeft() ? -1 : 1) * mDialogView.getWidth() / 2.0f);
+                mDialogView.setTranslationX(getDialogTranslation());
             }
             mDialogView.setAlpha(0);
             mDialogView.animate()
@@ -925,6 +924,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private boolean isWindowGravityLeft() {
         return (mWindowGravity & Gravity.LEFT) == Gravity.LEFT;
+    }
+    
+    private float getDialogTranslation() { 
+        return (isWindowGravityLeft() ? -1 : 1) * mDialogView.getWidth() / 2.0f;
     }
 
     private void initDimens() {
@@ -2024,12 +2027,13 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     hideRingerDrawer();
                 }, 50));
         if (!shouldSlideInVolumeTray()) {
-            animator.translationX(
-                    (isWindowGravityLeft() ? -1 : 1) * mDialogView.getWidth() / 2.0f);
+            animator.translationX(getDialogTranslation());
         }
 
         animator.setListener(getJankListener(getDialogView(), TYPE_DISMISS,
                 mDialogHideAnimationDurationMs)).start();
+
+        mDialogView.setTranslationX(getDialogTranslation());
 
         checkODICaptionsTooltip(true);
         synchronized (mSafetyWarningLock) {
