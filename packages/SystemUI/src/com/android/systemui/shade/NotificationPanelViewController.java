829435b8b687b6e2b58b13f9f4ab8b5f9dd7d840
ifloraxd <ifloraxd@gmail.com>
2024-07-04 16:55:04 +0000

    base: remove duplicate powemanagerinternal in QS Panel
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -652,7 +652,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private boolean mForceFlingAnimationForTest = false;
 
     private boolean mShowDimissButton;
-    private final PowerManagerInternal mLocalPowerManager;
 
     private boolean mBlockedGesturalNavigation = false;
 
