0a0c5cc694326bcd33ab25fd2228a1ef7cd6ee65
ifloraxd <ifloraxd@gmail.com>
2024-07-05 15:22:57 +0000

    Revert "SystemUI: Introduce Depth Wallpaper feature [1/2]"
    This reverts commit d08d44598da568e2f0b2455d03280712e4fabe10.
    
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -658,10 +658,6 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     public ScrimState getState() {
         return mState;
     }
-    
-    public float getScrimBehindAlpha() {
-        return mScrimBehindAlphaKeyguard;
-    }
 
     /**
      * Sets the additional scrim behind alpha keyguard that would be blended with the default scrim
@@ -1165,7 +1161,6 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                         AlarmTimeout.MODE_IGNORE_IF_SCHEDULED);
             });
         }
-        com.android.systemui.util.WallpaperDepthUtils.getInstance(mScrimBehind.getContext()).updateDepthWallpaper();
     }
 
     /**
