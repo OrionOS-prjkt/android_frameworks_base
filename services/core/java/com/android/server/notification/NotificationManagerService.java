60d981f8670ad550fc788a886e99b3dabedbd8c2
romiyusnandar <yusromi04@gmail.com>
2024-07-20 20:06:25 +0700

    Revert "Allow to suppress notifications sound/vibration if screen is ON [1/2]"
    This reverts commit 43ee40da2b3315fdcce6c1b40ea294675d530d96.
    
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -778,8 +778,6 @@ public class NotificationManagerService extends SystemService {
     // Broadcast intent receiver for notification permissions review-related intents
     private ReviewNotificationPermissionsReceiver mReviewNotificationPermissionsReceiver;
 
-    private boolean mSoundVibScreenOn;
-
     private AppLockManagerServiceInternal mAppLockManagerService = null;
 
     static class Archive {
@@ -2176,8 +2174,6 @@ public class NotificationManagerService extends SystemService {
                 = Settings.Secure.getUriFor(Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS);
         private final Uri SHOW_NOTIFICATION_SNOOZE
                 = Settings.Secure.getUriFor(Settings.Secure.SHOW_NOTIFICATION_SNOOZE);
-        private final Uri NOTIFICATION_SOUND_VIB_SCREEN_ON
-                = Settings.System.getUriFor(Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON);
 
         SettingsObserver(Handler handler) {
             super(handler);
@@ -2206,8 +2202,6 @@ public class NotificationManagerService extends SystemService {
                     false, this, UserHandle.USER_ALL);
 
             resolver.registerContentObserver(SHOW_NOTIFICATION_SNOOZE,
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(NOTIFICATION_SOUND_VIB_SCREEN_ON,
                     false, this, UserHandle.USER_ALL);
 
             update(null);
@@ -2261,11 +2255,6 @@ public class NotificationManagerService extends SystemService {
                 if (!snoozeEnabled) {
                     unsnoozeAll();
                 }
-            }
-            if (uri == null || NOTIFICATION_SOUND_VIB_SCREEN_ON.equals(uri)) {
-                mSoundVibScreenOn = Settings.System.getIntForUser(resolver,
-                        Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON, 1,
-                        UserHandle.USER_CURRENT) == 1;
             }
         }
 
@@ -9257,8 +9246,7 @@ public class NotificationManagerService extends SystemService {
         }
 
         if (aboveThreshold && isNotificationForCurrentUser(record)) {
-            boolean skipSound = mScreenOn && !mSoundVibScreenOn;
-            if (mSystemReady && mAudioManager != null && !skipSound) {
+            if (mSystemReady && mAudioManager != null) {
                 Uri soundUri = record.getSound();
                 hasValidSound = soundUri != null && !Uri.EMPTY.equals(soundUri);
                 VibrationEffect vibration = record.getVibration();
