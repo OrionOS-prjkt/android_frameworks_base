3ecd372765944eb0a261fc65f37795fb04973092
Joey Huab <jhuab@edukasyon.ph>
2024-08-20 23:44:57 +0900

    Add more bootanimation styles [1/3]
    * The classics are back
    
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -78,13 +78,19 @@ static const char PRODUCT_SHUTDOWNANIMATION_FILE[] = "/product/media/shutdownani
 static const char SYSTEM_SHUTDOWNANIMATION_FILE[] = "/system/media/shutdownanimation.zip";
 
 static const char* const BOOT_ANIMATION_FILES[] = {
-    "/product/media/bootanimation.zip",
-    "/product/media/bootanimation_ctos.zip",
-    "/product/media/bootanimation_cyberpunk.zip",
-    "/product/media/bootanimation_google.zip",
-    "/product/media/bootanimation_google_monet.zip",
-    "/product/media/bootanimation_valorant.zip",
-    "/data/misc/bootanim/bootanimation.zip"
+        "/product/media/bootanimation.zip",
+        "/product/media/bootanimation_aokp.zip",
+        "/product/media/bootanimation_cm.zip",
+        "/product/media/bootanimation_ctos.zip",
+        "/product/media/bootanimation_cyberpunk.zip",
+        "/product/media/bootanimation_du.zip",
+        "/product/media/bootanimation_google.zip",
+        "/product/media/bootanimation_google_monet.zip",
+        "/product/media/bootanimation_pac.zip",
+        "/product/media/bootanimation_rr.zip",
+        "/product/media/bootanimation_slim.zip",
+        "/product/media/bootanimation_valorant.zip",
+        "/data/misc/bootanim/bootanimation.zip"
 };
 
 static constexpr const char* PRODUCT_USERSPACE_REBOOT_ANIMATION_FILE = "/product/media/userspace-reboot.zip";
