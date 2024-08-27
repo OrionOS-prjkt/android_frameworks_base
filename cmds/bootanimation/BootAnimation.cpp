8e5fcd0f6020ea5772cf1b4320f6121ae60f9404
0xsharkboy <achille@0xsharkboy.dev>
2024-08-27 22:10:01 +0200

    Add ctOS bootanimation [1/3]
    Signed-off-by: 0xsharkboy <achille@0xsharkboy.dev>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -79,6 +79,7 @@ static const char SYSTEM_SHUTDOWNANIMATION_FILE[] = "/system/media/shutdownanima
 
 static const char* const BOOT_ANIMATION_FILES[] = {
     "/product/media/bootanimation.zip",
+    "/product/media/bootanimation_ctos.zip",
     "/product/media/bootanimation_cyberpunk.zip",
     "/product/media/bootanimation_google.zip",
     "/product/media/bootanimation_google_monet.zip",
@@ -776,9 +777,9 @@ void BootAnimation::findBootAnimationFile() {
     char value[PROPERTY_VALUE_MAX];
     property_get("persist.sys.bootanimation_style", value, "0");
     const int bootAnimStyle = atoi(value);
-    const char* selectedBootAnimation = 
-        (bootAnimStyle >= 0 && bootAnimStyle < 6) 
-        ? BOOT_ANIMATION_FILES[bootAnimStyle] 
+    const char* selectedBootAnimation =
+        (bootAnimStyle >= 0 && bootAnimStyle < sizeof(BOOT_ANIMATION_FILES) / sizeof(BOOT_ANIMATION_FILES[0]))
+        ? BOOT_ANIMATION_FILES[bootAnimStyle]
         : BOOT_ANIMATION_FILES[0];
     static const std::vector<std::string> bootFiles = {
         std::string(selectedBootAnimation)
