f8fd10ba430b18683595bc33b6ed0b1f30274c77
Pranav Vashi <neobuddy89@gmail.com>
2024-09-20 22:40:37 +0530

    More improvements for three finger swipe
    Ref: https://github.com/yaap/frameworks_base/commit/499273f81d8d3dcbd21675fb15c08edadf1c11a8
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -20901,7 +20901,13 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     @Override
     public boolean isThreeFingersSwipeActive() {
-        return mThreeFingersSwipeEnabled;
+        if (!mThreeFingersSwipeEnabled)
+            return false;
+        synchronized (this) {
+            return Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.THREE_FINGER_GESTURE_ACTIVE, 0,
+                    UserHandle.USER_CURRENT) == 1;
+        }
     }
 
     @Override
