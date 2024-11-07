697f989dc8b1a70277979eabfafaff87552780b4
Drenzzz <116653610+drenzzz@users.noreply.github.com>
2024-11-08 05:09:09 +0700

    Introduce bootanimation styles [1/3]  (#12)
    * bootanimation: Disable themed animation
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    
    * Introduce bootanimation styles [1/3]
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    
    * BootAnimation: Add option for custom bootanimation
    
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    
    * Bootanimation: Use property_get for retrieving bootanimation styles property
    
    * hopefully this fixes the issue where bootanimation styles cant be retrieve during an upgrade/new firmware flash
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    
    * Add ctOS bootanimation [1/3]
    
    Signed-off-by: 0xsharkboy <achille@0xsharkboy.dev>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    
    * Add more bootanimation styles [1/3]
    
    * The classics are back
    
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    
    ---------
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: 0xsharkboy <achille@0xsharkboy.dev>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Alvin Francis <nivlafx@gmail.com>
    Co-authored-by: 0xsharkboy <achille@0xsharkboy.dev>
    Co-authored-by: Joey Huab <jhuab@edukasyon.ph>

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -71,12 +71,27 @@ using ui::DisplayMode;
 
 static const char OEM_BOOTANIMATION_FILE[] = "/oem/media/bootanimation.zip";
 static const char PRODUCT_BOOTANIMATION_DARK_FILE[] = "/product/media/bootanimation-dark.zip";
-static const char PRODUCT_BOOTANIMATION_FILE[] = "/product/media/bootanimation.zip";
 static const char SYSTEM_BOOTANIMATION_FILE[] = "/system/media/bootanimation.zip";
 static const char APEX_BOOTANIMATION_FILE[] = "/apex/com.android.bootanimation/etc/bootanimation.zip";
 static const char OEM_SHUTDOWNANIMATION_FILE[] = "/oem/media/shutdownanimation.zip";
 static const char PRODUCT_SHUTDOWNANIMATION_FILE[] = "/product/media/shutdownanimation.zip";
 static const char SYSTEM_SHUTDOWNANIMATION_FILE[] = "/system/media/shutdownanimation.zip";
+
+static const char* const BOOT_ANIMATION_FILES[] = {
+        "/product/media/bootanimation.zip",
+        "/product/media/bootanimation_aokp.zip",
+        "/product/media/bootanimation_cm.zip",
+        "/product/media/bootanimation_ctos.zip",
+        "/product/media/bootanimation_cyberpunk.zip",
+        "/product/media/bootanimation_du.zip",
+        "/product/media/bootanimation_google.zip",
+        "/product/media/bootanimation_google_monet.zip",
+        "/product/media/bootanimation_pac.zip",
+        "/product/media/bootanimation_rr.zip",
+        "/product/media/bootanimation_slim.zip",
+        "/product/media/bootanimation_valorant.zip",
+        "/data/misc/bootanim/bootanimation.zip"
+};
 
 static constexpr const char* PRODUCT_USERSPACE_REBOOT_ANIMATION_FILE = "/product/media/userspace-reboot.zip";
 static constexpr const char* OEM_USERSPACE_REBOOT_ANIMATION_FILE = "/oem/media/userspace-reboot.zip";
@@ -765,10 +780,15 @@ bool BootAnimation::findBootAnimationFileInternal(const std::vector<std::string>
 
 void BootAnimation::findBootAnimationFile() {
     ATRACE_CALL();
-    const bool playDarkAnim = android::base::GetIntProperty("ro.boot.theme", 0) == 1;
+    char value[PROPERTY_VALUE_MAX];
+    property_get("persist.sys.bootanimation_style", value, "0");
+    const int bootAnimStyle = atoi(value);
+    const char* selectedBootAnimation =
+        (bootAnimStyle >= 0 && bootAnimStyle < sizeof(BOOT_ANIMATION_FILES) / sizeof(BOOT_ANIMATION_FILES[0]))
+        ? BOOT_ANIMATION_FILES[bootAnimStyle]
+        : BOOT_ANIMATION_FILES[0];
     static const std::vector<std::string> bootFiles = {
-        APEX_BOOTANIMATION_FILE, playDarkAnim ? PRODUCT_BOOTANIMATION_DARK_FILE : PRODUCT_BOOTANIMATION_FILE,
-        OEM_BOOTANIMATION_FILE, SYSTEM_BOOTANIMATION_FILE
+        std::string(selectedBootAnimation)
     };
     static const std::vector<std::string> shutdownFiles = {
         PRODUCT_SHUTDOWNANIMATION_FILE, OEM_SHUTDOWNANIMATION_FILE, SYSTEM_SHUTDOWNANIMATION_FILE, ""
